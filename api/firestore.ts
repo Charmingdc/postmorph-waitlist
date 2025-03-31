450d1cbbc38bc1965f18e3d02b2b75ce8bbecdb2
Adebayo Muis <charmingdc002@gmail.com>
2025-03-31 12:27:24 +0100

    Allow request from localhost temporarily

--- api/firestore.ts
+++ api/firestore.ts
@@ -4,12 +4,12 @@ import { getFirestore, doc, getDoc, setDoc, serverTimestamp } from "firebase/fir
 
 // Firebase config (from environment variables)
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
@@ -17,7 +17,7 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 // Allowed frontend domain
-const allowedOrigin = "https://contentflip-waitlist.vercel.app";
+const allowedOrigin = "http://localhost:5173/";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.headers.origin !== allowedOrigin) return res.status(403).json({ error: "Access Denied" });
