a095a6999b74c6516bb5c6ddfeca9fb71f86a93e
Adebayo Muis <charmingdc002@gmail.com>
2025-04-09 19:32:42 +0100

    Update meta data

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,10 @@ const Navbar = () => {
   <nav>
    <div className="w-screen flex items-center justify-start px-[2vw] mt-2">
      <img 
-       src="/contentflip-logo.png"
+       src="/postmorph-logo.png"
        width="50px"
        height="50px"
-       alt="Contentflip Logo"
+       alt="Postmorph Logo"
        loading="eager" />
          
      <h1 className="heading-gradient font-bold text-xl"> 
